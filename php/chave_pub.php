f48e4b0964df653c3583b03a8ef710e3b1e88188
Nicolas Ortiz <90704818+Nicolasortiiz@users.noreply.github.com>
2025-03-31 13:41:44 -0300

    Alteração no funcionamento da geração da chave pública
    chave pública não é gerada a partir de um ID

--- php/chave_pub.php
+++ php/chave_pub.php
@@ -1,5 +1,5 @@
 <?php
-$pubkey = shell_exec("python3 publica.py " . escapeshellarg("73613CFFF5D9EB2ABC8F66F7B0A5008C1950B989"));
+$pubkey = shell_exec("python3 publica.py");
 
 if($pubkey){
     echo json_encode(["pubkey" => htmlspecialchars($pubkey)]);
